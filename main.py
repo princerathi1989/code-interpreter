f3cf3ba5dd631f170a111964c677c8b28a3948db
Prince Chaudhary <princerathi1989@gmail.com>
2025-09-04 23:32:41 +0530

    router-agent

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from typing import Any
+
 from dotenv import load_dotenv
+from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 from langchain import hub
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+
 load_dotenv()
 
 def main():
@@ -10,6 +15,7 @@ def main():
 
     instructions = """You are an agent designed to write and execute python code to answer questions.
         You have access to a python REPL, which you can use to execute python code.
+        You have qrcode package installed
         If you get an error, debug your code and try again.
         Only use the output of your code to answer the question. 
         You might know the answer without running any code, but you should still run the code to get the answer.
@@ -20,22 +26,66 @@ def main():
 
     tools = [PythonREPLTool()]
 
-    agent = create_react_agent(
+    python_agent = create_react_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
         tools = tools,
         prompt=prompt
     )
 
-    agent_executor = AgentExecutor(
-        agent = agent,
+    python_agent_executor = AgentExecutor(
+        agent = python_agent,
         tools=tools,
         verbose=True,
     )
 
-    agent_executor.invoke(input={
-        "input": """generate and save in current working directory 15 QRcodes
-            that point to www.udemy.com/course/langchain, you have qrcode package installed already """
-    })
+    csv_agent_executor: AgentExecutor = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        verbose=True,
+        allow_dangerous_code=True
+    )
+
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str, Any]:
+        return python_agent_executor.invoke({"input": original_prompt})
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor_wrapper,
+            description="""useful when you need to transform natural language to python and execute the python code,
+                              returning the results of the code execution
+                              DOES NOT ACCEPT CODE AS INPUT""",
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent_executor.invoke,
+            description="""useful when you need to answer question over episode_info.csv file,
+                             takes an input the entire question and returns the answer after running pandas calculations""",
+        ),
+    ]
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools,
+    )
+    grand_agent_executor = AgentExecutor(agent=grand_agent, tools=tools, verbose=True)
+
+    print(
+        grand_agent_executor.invoke(
+            {
+                "input": "which season has the most episodes?",
+            }
+        )
+    )
+
+    print(
+        grand_agent_executor.invoke(
+            {
+                "input": "Generate and save in current working directory 15 qrcodes that point to `www.udemy.com/course/langchain`",
+            }
+        )
+    )
 
 if __name__ == "__main__":
     main()
